0577b6a166f0e0d1e321d02d5a48b5762acb2f93
srimanivinay97 <srimanivinay97@gmail.com>
2026-09-13 12:35:44 +0530

    Move Android configuration to app module

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,21 +3,3 @@ plugins {
     id("org.jetbrains.kotlin.android") version "2.3.21" apply false
 }
 
-android {
-    namespace = "com.vinay.androidagent"
-    compileSdk = 36
-
-    defaultConfig {
-        applicationId = "com.vinay.androidagent"
-
-        // Redmi 9i is old enough that we keep this low.
-        minSdk = 23
-        targetSdk = 36
-
-        versionCode = 1
-        versionName = "1.0"
-    }
-}
-
-dependencies {
-}
